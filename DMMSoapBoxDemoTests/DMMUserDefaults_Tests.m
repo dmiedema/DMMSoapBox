8274504b1e03cdabec228b42d2cb6ef4304b5dfb
dmiedema <daniel@househappy.org>
2015-02-02 09:53:53 -0800

    Add test files & travis. Need to write the tests still tho before travis integration

---
+++ DMMSoapBoxDemoTests/DMMUserDefaults_Tests.m
@@ -0,0 +1,42 @@
+//
+//  DMMUserDefaults_Tests.m
+//  DMMSoapBoxDemo
+//
+//  Created by Daniel on 2/2/15.
+//  Copyright (c) 2015 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import "DMMUserDefaults.h"
+
+@interface DMMUserDefaults_Tests : XCTestCase
+
+@end
+
+@implementation DMMUserDefaults_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
